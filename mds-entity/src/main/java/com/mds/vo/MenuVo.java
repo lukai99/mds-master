721440abb81b82b8d23a61bcc65b9ffab0f27e2d
tomarot <1016069049@qq.com>
2018-03-21 21:18:21 +0800

    创建vo包

---
+++ mds-entity/src/main/java/com/mds/vo/MenuVo.java
@@ -0,0 +1,13 @@
+package com.mds.vo;
+
+import com.mds.entity.Menu;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: T5S
+ * Date: 2018/3/21
+ * Time: 21:16
+ * To change this template use File | Settings | File Templates.
+ */
+public class MenuVo extends Menu {
+}
